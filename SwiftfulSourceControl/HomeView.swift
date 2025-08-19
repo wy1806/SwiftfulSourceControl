52d4560d2a3e25b7a5d2984a0aec98c1892b9d77
Yu Wang <wy_1806@hotmail.com>
2025-08-19 16:41:57 +0800

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by WangYu on 2025/8/19.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
