2c814a524ac8a3b53b83561ac352f143858b8eb6
Yu Wang <wy_1806@hotmail.com>
2025-08-19 20:24:36 +0800

    [Bug] Fix Swiftful Thinking text

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -42,7 +42,7 @@ struct ContentView: View {
             Image(systemName: "heart.fill")
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
-            Text("Swiftful!")
+            Text("Swiftful Thinking!")
             
             Button("Subscribe") {
                 
