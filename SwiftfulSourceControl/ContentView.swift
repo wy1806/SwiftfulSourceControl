a32fd4c0217a3d2a994602e71a33800264a7dad3
Yu Wang <wy_1806@hotmail.com>
2025-08-19 20:10:28 +0800

    [Clean] Updates to documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -6,6 +6,14 @@
 //
 
 /*
+ Clone = Copying the repo locally
+ Commit = Save ("Checkpoint") on our current branch
+ Stage = Prepare changes for a commit
+ Stash = Save changes for later
+ Push = Send local commits to remote repo
+ Pull = Fetch remote commits to local repo
+ 
+
  COMMIT MESSAGES
  
  NEW FEATURE:
