b35360fd638f047958dba8c988755ee16f3c73f8
Yu Wang <wy_1806@hotmail.com>
2025-08-20 17:57:25 +0800

    [Feature] Title to Hello

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     
-    @State private var title : String = "Hello, world!"
+    @State private var title : String = "Hello"
     
     var body: some View {
         VStack {
