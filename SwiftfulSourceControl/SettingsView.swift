5d8b4499edfc821940d462f62c5b5a537d3ebe76
Yu Wang <wy_1806@hotmail.com>
2025-08-20 22:52:11 +0800

    [Feature] Add SettingsView

---
+++ SwiftfulSourceControl/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SwiftfulSourceControl
+//
+//  Created by WangYu on 2025/8/20.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings!")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
