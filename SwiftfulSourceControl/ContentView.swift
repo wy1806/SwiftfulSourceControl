8a03fe275e899d5ad3e25f9f1d2d443e15d644fe
Yu Wang <wy_1806@hotmail.com>
2025-08-20 22:23:05 +0800

    [Feature] Add finalize title for screen

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -48,7 +48,7 @@ struct ContentView: View {
                         Image(systemName: "globe")
                             .font(.largeTitle)
                             .foregroundStyle(.tint)
-                        Text("Swiftful Thinking!")
+                        Text("Swiftful!!!!!!!")
                         
                         Button("Subscribe now!") {
                         }
