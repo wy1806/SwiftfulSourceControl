741272f5989eeabbf1ef8ab7805e222e81d4ba03
Yu Wang <wy_1806@hotmail.com>
2025-08-21 21:21:36 +0800

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by WangYu on 2025/8/21.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Nick!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
